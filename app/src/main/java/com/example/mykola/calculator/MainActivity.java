526b6ce29d46bbd1908966a021d042537d961299
mykolamatsiakh <mmatsiakh@gmail.com>
2016-12-17 17:50:55 +0200

    refactored code

--- app/src/main/java/com/example/mykola/calculator/MainActivity.java
+++ app/src/main/java/com/example/mykola/calculator/MainActivity.java
@@ -1,111 +1,97 @@
 package com.example.mykola.calculator;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static Intent getStartIntent(Context context) {
-        Intent startIntent = new Intent(context, MainActivity.class);
-        return startIntent;
-    }
-    private EditText mFirstNumber;
-    private EditText mSecondNumber;
-    private Button mSum;
-    private Button mDifference;
-    private Button mDivision;
-    private Button mMulti;
 
+    private EditText mFirstNumberEditText;
+    private EditText mSecondNumberEditText;
+    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirstNumber=(EditText) findViewById(R.id.first_number);
-        mSecondNumber=(EditText) findViewById(R.id.second_number);
-        mMulti=(Button) findViewById(R.id.multi);
-        mDivision=(Button) findViewById(R.id.division);
-        mDifference=(Button) findViewById(R.id.difference);
-        mSum=(Button) findViewById(R.id.sum);
-        mSum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSum();
-            }
-        });
-        mMulti.setOnClickListener(new View.OnClickListener() {
+
+        mFirstNumberEditText = (EditText) findViewById(R.id.first_number);
+        mSecondNumberEditText = (EditText) findViewById(R.id.second_number);
+        mResultTextView = (TextView)findViewById(R.id.result);
+
+        Button multiButton = (Button) findViewById(R.id.multi);
+        multiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showMulti();
             }
         });
-        mDivision.setOnClickListener(new View.OnClickListener() {
+
+        Button divisionButton = (Button) findViewById(R.id.division);
+        divisionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDivision();
             }
         });
-        mDifference.setOnClickListener(new View.OnClickListener() {
+
+        Button differenceButton = (Button) findViewById(R.id.difference);
+        differenceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDifference();
             }
         });
+
+        Button sumButton = (Button) findViewById(R.id.sum);
+        sumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSum();
+            }
+        });
     }
 
-    private int getFirstNumber()
-    {
-        validateFirstNumber();
-        return Integer.parseInt(mFirstNumber.getText().toString().trim());
+    private void showSum() {
+        showResult(getFirstNumber() + getSecondNumber());
     }
 
-    private int getSecondNumber()
-    {
-        validateSecondNumber();
-        return Integer.parseInt(mSecondNumber.getText().toString().trim());
+    private void showDifference() {
+        showResult(getFirstNumber() - getSecondNumber());
     }
 
-    private void showSum(){
-        showResult(getFirstNumber()+getSecondNumber());
+    private void showDivision() {
+        showResult(getFirstNumber() / getSecondNumber());
     }
 
-    private void showDifference(){
-        showResult(getFirstNumber()-getSecondNumber());
+    private void showMulti() {
+        showResult(getFirstNumber() * getSecondNumber());
     }
 
-    private void showDivision(){
-        showResult(getFirstNumber()/getSecondNumber());
+    private void showResult(double result) {
+       mResultTextView.setText(Double.toString(result));
     }
 
-    private void showMulti(){
-        showResult(getFirstNumber()*getSecondNumber());
-    }
-
-    private void showResult(Integer message){
-        Toast.makeText(MainActivity.this, message.toString(), Toast.LENGTH_LONG).show();
-    }
-    private int validateFirstNumber(){
-        if(mFirstNumber== null)
-        {
+    private double getFirstNumber() {
+        String number = mFirstNumberEditText.getText().toString().trim();
+        if(TextUtils.isEmpty(number)){
             return 0;
         }
-        else
-            return getFirstNumber();
-
+        return Double.parseDouble(number);
     }
-    private int validateSecondNumber(){
-        if(mSecondNumber==null){
-            return 0;
+
+
+    private double getSecondNumber() {
+        String number = mSecondNumberEditText.getText().toString();
+        if (TextUtils.isEmpty(number)) {
+            return  0;
         }
-        else
-            return getSecondNumber();
+
+        return Double.parseDouble(mSecondNumberEditText.getText().toString().trim());
     }
-
-
 }
